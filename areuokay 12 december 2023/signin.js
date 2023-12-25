db63ac3511c0d0111e0fab318fe0a673ef712687
MikeKomari <michaelk.projects@gmail.com>
2023-12-25 16:21:34 +0700

    before rapihin

---
+++ areuokay 12 december 2023/signin.js
@@ -0,0 +1,14 @@
+let db = [
+    {
+        email: "mike@gmail.com",
+        password: "123",
+    },
+    {
+        email: "haerin@gmail.com",
+        password: "1505",
+    }
+];
+
+
+const emailInput = document.querySelector(".email_input");
+const passwordInput = document.querySelector(".password_input");
